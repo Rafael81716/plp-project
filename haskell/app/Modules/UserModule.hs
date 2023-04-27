f3338203aec9ca4f21733ca3fe54a23f827f2772
GabrielYuriRF0 <gabrie.yuri1020@gmail.com>
2023-04-26 22:28:13 -0300

    Make user register

--- haskell/app/Modules/UserModule.hs
+++ haskell/app/Modules/UserModule.hs
@@ -9,6 +9,7 @@ import GHC.Generics
 import System.IO.Unsafe
 import System.IO
 import System.Directory
+import Data.List.Split
 
 data User = User{
         name::String,
@@ -17,7 +18,23 @@ data User = User{
         bookGenres:: [String]
     } deriving(Show,Eq,Read)
 
+
 registerUser:: String -> String -> String -> [String] -> IO()
 registerUser readName readEmail readPassword readGenres = do
     let user = User readName readEmail readPassword readGenres
-    putStrLn $ show user
+    let user1 = [user]
+    putStrLn $  show user
+    saveCsv user1 "users.csv"
+
+
+saveCsv :: (Show u) => [u] -> FilePath -> IO ()
+saveCsv valores caminhoArquivo = appendFile caminhoArquivo conteudoCSV
+  where conteudoCSV = unlines $ map show valores
+
+parsePessoa :: String -> User
+parsePessoa linhaCSV = User name email password bookGenres
+  where [name, email, password, bookGenres] = splitOn "," linhaCSV
+        
+
+        
+ 
