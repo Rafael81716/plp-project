2f49dc66658b7f8fe99f35a926ddf39bf8b246f0
joaovictorsl <joaovictordev2003@gmail.com>
2023-04-29 21:56:44 -0300

    Creating CSV module

---
+++ haskell/app/Modules/CsvModule.hs
@@ -0,0 +1,15 @@
+module Modules.CsvModule where
+import Util (wordsWhen)
+
+append :: (Show t) => [t] -> FilePath -> IO ()
+append values pathToFile = do
+    appendFile pathToFile csvContent
+  where
+    csvContent = unlines $ map show values
+
+read :: (Read t) => (String -> t) -> FilePath -> IO [t]
+read parser pathToFile = do
+    csvData <- readFile pathToFile
+    let lines = wordsWhen (== '\n') csvData
+    let dataList = map parser lines
+    return dataList
