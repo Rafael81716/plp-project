a2779fabedd49c83e3aac0a73f7cfa524e3e30d5
joaovictorsl <joaovictordev2003@gmail.com>
2023-05-07 01:55:55 -0300

    Updating Models

--- haskell/app/Model/Book.hs
+++ haskell/app/Model/Book.hs
@@ -1,11 +1,35 @@
 module Model.Book where
-data Book = Book{
-    num::Int,
-    name::String,
-    author::String,
-    genre::String,
-    link::String
-} deriving (Eq, Read)
+
+import Modules.UtilModule (splitBy)
+
+data Book = Book
+  { num :: Int,
+    name :: String,
+    author :: String,
+    genre :: String,
+    link :: String
+  }
+  deriving (Eq)
 
 instance Show Book where
-    show(Book num name author genre link) = show num ++ ";" ++name ++ ";" ++ author ++ ";" ++ genre ++ ";" ++ link
+  show (Book num n a g l) = show num ++ ";" ++ n ++ ";" ++ a ++ ";" ++ g ++ ";" ++ l
+
+instance Read Book where
+  readsPrec _ str = [(strToBook str, "")]
+
+bookToNumName :: Book -> String
+bookToNumName b = show (num b) ++ " - " ++ name b ++ "\n"
+
+strToBook :: String -> Book
+strToBook userStr = do
+  let attList = splitBy ';' userStr
+
+  let num = read (head attList) :: Int
+  let n = attList !! 1
+  let a = attList !! 2
+  let g = attList !! 3
+  let l = read (attList !! 4)
+  Book num n a g l
+
+formatBook :: Book -> String
+formatBook b = show (num b) ++ " - " ++ name b ++ " - " ++ author b ++ " (" ++ genre b ++ ")"
--- haskell/app/Model/User.hs
+++ haskell/app/Model/User.hs
@@ -1,34 +1,33 @@
 module Model.User where
 
-import Modules.UtilModule (parseStrToList, wordsWhen)
+import Modules.UtilModule (parseStrToList, splitBy)
 
 data User = User
-  { nameUser :: String,
+  { name :: String,
     email :: String,
     password :: String,
     bookGenres :: [String],
     favoriteBooks :: [Int],
     booksLoan :: [Int],
-    booksHistoric :: [Int]
+    recentBooks :: [Int]
   }
   deriving (Eq)
 
 instance Read User where
-  readsPrec _ str =
-    case wordsWhen (== ';') str of
-      [n, e, s, gs, fb, bk, bh] -> [(strToUser [n, e, s, gs, fb, bk, bh], "")]
-      _ -> []
+  readsPrec _ str = [(strToUser str, "")]
 
 instance Show User where
-  show (User name email password bookGenres favoriteBooks booksLoan booksHistoric) = name ++ ";" ++ email ++ ";" ++ password ++ ";" ++ Prelude.show bookGenres ++ ";" ++ Prelude.show favoriteBooks ++ ";" ++ Prelude.show booksLoan ++ ";" ++ Prelude.show booksHistoric
+  show (User n e p bG fB bL rB) = n ++ ";" ++ e ++ ";" ++ p ++ ";" ++ Prelude.show bG ++ ";" ++ Prelude.show fB ++ ";" ++ Prelude.show bL ++ ";" ++ Prelude.show rB
 
-strToUser :: [String] -> User
-strToUser x = do
-  let n = head x
-  let e = x !! 1
-  let s = x !! 2
-  let g = parseStrToList (x !! 3)
-  let f = read (x !! 4)
-  let b = read (x !! 5)
-  let a = read (x !! 6)
+strToUser :: String -> User
+strToUser userStr = do
+  let attList = splitBy ';' userStr
+
+  let n = head attList
+  let e = attList !! 1
+  let s = attList !! 2
+  let g = parseStrToList (attList !! 3)
+  let f = read (attList !! 4)
+  let b = read (attList !! 5)
+  let a = read (attList !! 6)
   User n e s g f b a
