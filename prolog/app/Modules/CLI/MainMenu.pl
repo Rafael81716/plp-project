e52f94a887d82cb1c1aa781ea31a048ead7f7fd6
GabrielYuriRF0 <gabriel.yuri1020@gmail.com>
2023-05-25 17:50:30 -0300

    feat: create main menu

---
+++ prolog/app/Modules/CLI/MainMenu.pl
@@ -0,0 +1,11 @@
+:- use_module("../UtilModule.pl").
+printMainMenu:-
+centeredText("Inicio",40),
+write("\n1 - Entrar\n2 - Cadastrar\nEscolha uma opcao: "),
+read(Option),
+readMainMenu(Option,R),
+write(R).
+
+readMainMenu(Option,"todo login"):- Option =:= 1,!.
+readMainMenu(Option,"todo register"):- Option =:= 2,!.
+readMainMenu(_,R):- clearScreen,write("Opcao invalida, digite novamente!\n"), printMainMenu.
