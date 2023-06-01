4d971331db14f02e1a39856f27a5d9ad5c70ac76
GabrielYuriRF0 <gabriel.yuri1020@gmail.com>
2023-06-01 18:09:52 -0300

    feat: finish check user register

--- prolog/app/Modules/CLI/LoginAndRegisterModule.pl
+++ prolog/app/Modules/CLI/LoginAndRegisterModule.pl
@@ -1,6 +1,7 @@
-:- module(LoginAndRegisterModule,[registerUserMenu/0]).
+:- module(LoginAndRegisterModule,[registerUserMenu/0, printGenres/0, checkEmail/1]).
 :- use_module('../UserModule.pl').
 :- use_module('../UtilModule.pl').
+:- use_module('../CsvModule.pl').
 
 registerUserMenu :-
 centeredText("Cadastre-se",40),
@@ -9,6 +10,10 @@ read(ReadName),
 
 write("\nDigite seu email: "),
 read(ReadEmail),
+getUsers(Users),
+checkUserRegister(ReadEmail, Users,IsValidEmail),
+checkEmail(IsValidEmail),
+
 write("\nDigite sua senha: "),
 read(ReadPassword),
 printGenres(),
@@ -21,6 +26,11 @@ printGenres:-
 clearScreen,
 centeredText("Cadastre-se",63),
 write("\nEscolha ate 5 generos literarios pelos seus respectivos numeros\nem ordem de preferencia e digite -1 para finzalizar a digitação:\n1 - Ficcao\n2 - Fantasia\n3 - Infantil\n4 - Misterio\n5 - Historia\n6 - Aventura\n7 - Romance\n").
+
+
+checkEmail('invalido'):- clearScreen,write("Este email ja esta cadastrado!\nEscolha outro:\n"), registerUserMenu.
+checkEmail('valido'):- !.
+
 
 
 
