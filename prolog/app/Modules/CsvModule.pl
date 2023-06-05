20e4faf72a0e5bd0ae81dca6b250ba89403997be
joaovictorsl <joaovictordev2003@gmail.com>
2023-06-05 19:32:56 -0300

    Makes all read operations under /Data

--- prolog/app/Modules/CsvModule.pl
+++ prolog/app/Modules/CsvModule.pl
@@ -4,11 +4,11 @@
 
 
 getUsers(Users) :- 
-ler_arquivo_csv('../users.csv', DadosT), 
+ler_arquivo_csv('./Data/users.csv', DadosT), 
 len(DadosT, R),
 atualizaUsers(DadosT, R, 1, Users).
 
-getBooks(X) :- ler_arquivo_csv('../Data/books.csv', X).
+getBooks(X) :- ler_arquivo_csv('./Data/books.csv', X).
 
 ler_arquivo_csv(NomeArquivo, Dados) :-
     open(NomeArquivo, read, Arquivo),
