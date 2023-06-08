127edf13a36fbc6537a53c8184216efb7787ca96
Rafael <rafacavalcante081@gmail.com>
2023-06-08 20:07:35 -0300

    start to test

--- prolog/app/Modules/CLI/Historic.pl
+++ prolog/app/Modules/CLI/Historic.pl
@@ -2,12 +2,24 @@
 
 printAddToHistoric(User, Book) :-
     nth1(7, User, Historic),
-    checkIsValidSize(User, Historic),
-
+    nth1(2, User, ActualEmail),
+    checkIsValidSize(User, Historic, ActualHistoric),
+    attUserHistoric(User, ActualHistoric),
+    getUsers(Users),
+    checkUserRegister(ActualEmail, Users, NewUsers),
+    nth1(1,NewUsers, NewUser),
+    
     writeln('Voce leu esse livro?\n'),
     write('Digite <1> para Sim ou <2> para Não: '),
     read(IsRead),
     nth1(1, Book, Id),
-    bookHistoric(User, Id, IsRead),!.
+    bookHistoric(NewUser, Id, IsRead),!.
 
-checkIsValidSize(User, Historic):-!.
+checkIsValidSize(User, Historic, ActualHistoric):-
+    length(Historic, Size),
+    Size <= 9,
+    ActualHistoric = Historic, !.
+checkIsValidSize(User, [H|T], ActualHistoric) :-
+    length(Historic, Size),
+    Size >= 10,
+    ActualHistoric = T,!.
