b8c44f39c713a356ad84e03cb752ae7d3d86089d
Rafael <rafacavalcante081@gmail.com>
2023-06-09 22:47:05 -0300

    create users.csv if not exists

--- prolog/app/Data/Data.pl
+++ prolog/app/Data/Data.pl
@@ -1,6 +1,14 @@
-:- module(DataModule,[caminhar_ate_diretorio_atual/1]).
+:- module(DataModule,[caminhar_ate_diretorio_atual/1, verifyFileExists/1]).
 :- use_module(library(csv)).
+:- use_module(library(filesex)).
 
 caminhar_ate_diretorio_atual(Diretorio) :-
     source_file(Predicado, Arquivo),
     file_directory_name(Arquivo, Diretorio),!.
+
+verifyFileExists(Name) :-
+    exists_file(Name), nl.
+verifyFileExists(Name) :-
+    \+ exists_file(Name),
+    open(Name, write, Stream),
+    close(Stream), nl.
