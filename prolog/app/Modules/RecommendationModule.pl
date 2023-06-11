3bf712e1cd43568e6857cc7e0d1c82110b5995c7
joaovictorsl <joaovictordev2003@gmail.com>
2023-06-10 23:53:03 -0300

    Recommendation done

--- prolog/app/Modules/RecommendationModule.pl
+++ prolog/app/Modules/RecommendationModule.pl
@@ -4,18 +4,47 @@
 
 
 recommendation(User, Recommendation):-
-    nth0(6, User, ForbbidenBooksId),
-    getBooksById(ForbbidenBooksId, ForbbidenBooks),
+    nth0(6, User, ForbiddenBooksId),
     nth0(3, User, BooksGenres),
 
     (BooksGenres = [] ->
-    randomRecommendation(ForbbidenBooks, Recommendation)
-    ; custom_recommendation(ForbbidenBooks, Recommendation)).
+    randomRecommendation(ForbiddenBooksId, Recommendation)
+    ; customRecommendation(ForbiddenBooksId, BooksGenres, Recommendation)).
 
-randomRecommendation(ForbbidenBooks, Recommendation):-
-    generateRandomNumbers(10, 1, 204, RandomNumbers),
-    getBooksById(RandomNumbers, RandomBooks),
-    removeDoubles(RandomBooks, ForbbidenBooks, Recommendation).
+getNotForbiddenList(ForbiddenBooksId, SafeAllIds):-
+    range(1, 204, AllIds),
+    removeDoubles(AllIds, ForbiddenBooksId, SafeAllIds).
 
-customRecommendation(ForbbidenBooks, Recommendation):-
-    write('TODO').
+randomRecommendation(ForbiddenBooksId, Recommendation):-
+    getNotForbiddenList(ForbiddenBooksId, SafeAllIds),
+    getRandomElements(SafeAllIds, 10, RandomIds),
+    getBooksById(RandomIds, Recommendation).
+
+customRecommendation(ForbiddenBooksId, BooksGenres, Recommendation):-
+    getNotForbiddenList(ForbiddenBooksId, SafeAllIds),
+    getBooksById(SafeAllIds, BookSource),
+    
+    length(BooksGenres, GenreAmount),
+    AmountPerGenre is max(1, 10 // GenreAmount),
+    
+    getRandomBooksFromGenres(BookSource, BooksGenres, AmountPerGenre, TempRecommendation),
+    
+    length(TempRecommendation, Len),
+    nth0(0, BooksGenres, FavoriteGenre),
+    MissingBooks is 10 - Len,
+    
+    (MissingBooks > 0 ->
+    getRandomBooksByGenre(BookSource, FavoriteGenre, MissingBooks, RemainingBooks),
+    append(RemainingBooks, TempRecommendation, Recommendation)
+    ;
+    Recommendation = TempRecommendation).
+
+getRandomBooksFromGenres(_, [], _, []):-!.
+getRandomBooksFromGenres(BookSource, [H | T], AmountPerGenre, RandomBooksByGenre):-
+    getRandomBooksByGenre(BookSource, H, AmountPerGenre, BooksByGenre),
+    getRandomBooksFromGenres(BookSource, T, AmountPerGenre, RandomBooksByGenreAux),
+    append(BooksByGenre, RandomBooksByGenreAux, RandomBooksByGenre),!.
+
+getRandomBooksByGenre(BookSource, Genre, Amount, Books):-
+    getBooksFromSourceByGenre(BookSource, Genre, BooksByGenre),
+    getRandomElements(BooksByGenre, Amount, Books),!.
